85d033fd09d137eb98b02b2e7348981424cceb9e
Derek Weitzel <djw8605@gmail.com>
2019-12-09 11:23:49 -0600

    Debugging statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ site_dir=$1
 
 # Start the nginx server
 rsync -av $site_dir/ /usr/share/nginx/html/
+ls -l /usr/share/nginx/html/
 nginx &
 sleep 1
 
 # Start the linkchecker
 /root/.pub-cache/bin/linkcheck :80
+
+curl localhost:80
